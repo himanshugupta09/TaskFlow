5c5d6ed4eb43c86e55ced6dcd8ac9cb57cc058c9
himanshugupta09 <hg7984152@gmail.com>
2026-08-25 00:07:20 +0530

    lint fix

--- taskflow/manage.py
+++ taskflow/manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskflow.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
--- taskflow/taskflow/wsgi.py
+++ taskflow/taskflow/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for taskflow project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskflow.settings')
 
 application = get_wsgi_application()
+app = application
