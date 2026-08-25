91228569b10c51734942c8be7e36037ba5b2a9fc
himanshugupta09 <hg7984152@gmail.com>
2026-08-25 09:44:40 +0530

    db fix

--- taskflow/taskflow/settings.py
+++ taskflow/taskflow/settings.py
@@ -54,12 +54,23 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'taskflow.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = []
 
